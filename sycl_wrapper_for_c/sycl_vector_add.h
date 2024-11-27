eb9180c5354d4b38001ceadd8742fe97e5c92552
kazuki minemura <kazuki.minemura@gmail.com>
2024-11-27 14:18:30 +0900

    Create sycl_vector_add.h

---
+++ sycl_wrapper_for_c/sycl_vector_add.h
@@ -0,0 +1,15 @@
+// vector_add.h
+#ifndef VECTOR_ADD_H
+#define VECTOR_ADD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void vector_add(const float* A, const float* B, float* C, size_t n);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // VECTOR_ADD_H
