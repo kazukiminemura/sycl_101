2a2e0c2abf2ad541cbd2d490aac2d77955941eca
Kazuki Minemura <kazuki.minemura@gmail.com>
2025-03-07 21:34:26 +0900

    fizxed

--- sycl_atomic_bench/sycl_atomic_bench.cpp
+++ sycl_atomic_bench/sycl_atomic_bench.cpp
@@ -2,24 +2,36 @@
 #include <chrono>
 
 using namespace sycl;
-constexpr size_t N = 1024;
+constexpr int N = 1024;
+constexpr int M = 16;
 
 int main(){
     queue q;
     std::cout << q.get_device().get_info<info::device::name>() << std::endl;
-
+    
     // normal kernel
     {
         auto start = std::chrono::high_resolution_clock::now();
-        buffer<int> buf(N);
-        int M = 16;
+        // buffer<int> buf{range{N}};
+        buffer<int> buf(range{N});
         q.submit([&](handler& h){
             accessor acc{buf, h};
-            h.parallel_for(range(N), [=](id<1> i){
-                int j = i % M;
-                acc[j] = j;
+            h.parallel_for(N, [=](id<1> idx){
+                int j = idx % M;
+                acc[idx] = j;
             });
-        }).wait();
+        });
+
+        host_accessor host_acc{buf};
+        for(int i = 0; i < N; i++){
+            int j = i % M;
+            // std::cout << i << "-th :" << host_acc[i] << std::endl;
+            assert(host_acc[i] == j);
+            // if(host_acc[j] != i){
+            //     std::cout << "Error at index " << i << std::endl;
+            //     break;
+            // }
+        }
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> diff = end - start;
         std::cout << "Normal kernel execution time: " << diff.count() << " seconds" << std::endl;
@@ -29,16 +41,22 @@ int main(){
     {
         auto start = std::chrono::high_resolution_clock::now();
         buffer<int> buf(N);
-        int M = 16;
         q.submit([&](handler& h){
             accessor acc{buf, h};
             h.parallel_for(range(N), [=](id<1> i){
                 int j = i % M;
                 atomic_ref<int, memory_order::relaxed, memory_scope::system, 
-                    access::address_space::global_space> atomic_acc(acc[j]);
-                atomic_acc += 1;
+                    access::address_space::global_space> atomic_acc(acc[i]);
+                atomic_acc = j;
             });
-        }).wait();
+        });
+
+        host_accessor host_acc{buf};
+        for(int i = 0; i < N; i++){
+            int j = i % M;
+            assert(host_acc[i] == j);
+        }
+
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> diff = end - start;
         std::cout << "Atomic kernel execution with buffer time: " << diff.count() << " seconds" << std::endl;
@@ -48,18 +66,26 @@ int main(){
     {
         auto start = std::chrono::high_resolution_clock::now();
         int* data = malloc_shared<int>(N, q);
-        int M = 16;
         q.submit([&](handler& h){
             h.parallel_for(range(N), [=](id<1> i){
                 int j = i % M;
                 atomic_ref<int, memory_order::relaxed, memory_scope::system, 
-                    access::address_space::global_space> atomic_acc(data[j]);
-                atomic_acc += 1;
+                    access::address_space::global_space> atomic_acc(data[i]);
+                atomic_acc = j;
             });
-        }).wait();
+        });
+        q.wait();
+
+        for(int i = 0; i < N; i++){
+            int j = i % M;
+            assert(data[i] == j);
+        }
+
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> diff = end - start;
         std::cout << "Atomic kernel execution with USM time: " << diff.count() << " seconds" << std::endl;
+
+        free(data, q);
     }
 
 
