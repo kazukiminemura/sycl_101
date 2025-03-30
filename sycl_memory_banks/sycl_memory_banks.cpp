452cbff385dbdfa3f4c7c1d25b1dedcdf26b6d78
kazuki minemura <kazuki.minemura@gmail.com>
2025-03-30 21:18:46 +0900

    Update sycl_memory_banks.cpp

--- sycl_memory_banks/sycl_memory_banks.cpp
+++ sycl_memory_banks/sycl_memory_banks.cpp
@@ -74,35 +74,36 @@ int main() {
     }
 
     std::cout << "Bank conflicts" << std::endl;
-    for (int bank_count = 0; bank_count <= 32; bank_count++) {
-        auto start = std::chrono::high_resolution_clock::now();
+    {
+      auto start = std::chrono::high_resolution_clock::now();
+      int bank_count = 16;
 
-        q.submit([&](handler &h) {
-            sycl::local_accessor<int, 1> slm(sycl::range(32 * 64), h);
+      q.submit([&](handler &h) {
+          sycl::local_accessor<int, 1> slm(sycl::range(32 * 64), h);
 
-            h.parallel_for(sycl::nd_range(sycl::range{GLOBAL}, sycl::range{LOCAL}),
-                   [=](sycl::nd_item<1> it) {
-                     int i = it.get_global_linear_id();
-                     int j = it.get_local_linear_id();
+          h.parallel_for(sycl::nd_range(sycl::range{GLOBAL}, sycl::range{LOCAL}),
+                  [=](sycl::nd_item<1> it) {
+                    int i = it.get_global_linear_id();
+                    int j = it.get_local_linear_id();
 
+                    // access to 0-th bank only
+                    slm[j * bank_count] = 0;
+                    it.barrier(sycl::access::fence_space::local_space);
+  
+                    for (int m = 0; m < 1024 * 1024; m++) {
+                      slm[j * bank_count] += i * m;
+                      it.barrier(sycl::access::fence_space::local_space);
+                    }
+  
+                    data[i] = slm[j * bank_count];
 
-                     slm[j*bank_count] = 0;
-                     it.barrier(sycl::access::fence_space::local_space);
-    
-                     for (int m = 0; m < 1024 * 1024; m++) {
-                       slm[j*bank_count] += i * m;
-                       it.barrier(sycl::access::fence_space::local_space);
-                     }
-    
-                     data[i] = slm[j*bank_count];
+          });
+      }).wait();
 
-            });
-        }).wait();
+      auto end = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<double, std::milli> duration = end - start;
 
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double, std::milli> duration = end - start;
-
-        std::cout << "Banks: " << bank_count << " | Execution Time: " << duration.count() << " ms" << std::endl;
+      std::cout << "Execution Time: " << duration.count() << " ms" << std::endl;
     }
 
     return 0;
