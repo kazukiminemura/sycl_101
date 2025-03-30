443f00620127f0d080c28127b49d4207728d41c8
Kazuki Minemura <kazuki.minemura@gmail.com>
2025-03-30 20:39:13 +0900

    added

--- sycl_memory_banks/sycl_memory_banks.cpp
+++ sycl_memory_banks/sycl_memory_banks.cpp
@@ -7,8 +7,8 @@ using namespace sycl;
 
 int main() {
     queue q;
-    constexpr int GLOBAL = 64;
-    constexpr int LOCAL = 64;
+    constexpr int GLOBAL = 32;
+    constexpr int LOCAL = 32;
     int *data = sycl::malloc_shared<int>(GLOBAL, q);
 
     std::cout << "Local Memory Size for each work-group: "
@@ -16,11 +16,69 @@ int main() {
             << " Bytes" << std::endl;
 
 
-    for (int bank_count = 0; bank_count <= 64; bank_count++) {
+    std::cout << "warm-up" << std::endl;
+    {
+      // auto start = std::chrono::high_resolution_clock::now();
+      q.submit([&](handler &h) {
+        sycl::local_accessor<int, 1> slm(sycl::range(32 * 64), h);
+
+        // no-conflicts
+        h.parallel_for(sycl::nd_range(sycl::range{GLOBAL}, sycl::range{LOCAL}),
+               [=](sycl::nd_item<1> it) {
+                 int i = it.get_global_linear_id();
+                 int j = it.get_local_linear_id();
+
+                 slm[j] = 0;
+                 it.barrier(sycl::access::fence_space::local_space);
+
+                 for (int m = 0; m < 1024 * 1024; m++) {
+                   slm[j] += i * m;
+                   it.barrier(sycl::access::fence_space::local_space);
+                 }
+
+                 data[i] = slm[j];
+        });
+      }).wait();
+
+      // auto end = std::chrono::high_resolution_clock::now();
+      // std::chrono::duration<double, std::milli> duration = end - start;
+      // std::cout << "Execution Time: " << duration.count() << " ms" << std::endl;
+    }
+    std::cout << "without bank conflicts" << std::endl;
+    {
+      auto start = std::chrono::high_resolution_clock::now();
+      q.submit([&](handler &h) {
+        sycl::local_accessor<int, 1> slm(sycl::range(32 * 64), h);
+
+        // no-conflicts
+        h.parallel_for(sycl::nd_range(sycl::range{GLOBAL}, sycl::range{LOCAL}),
+               [=](sycl::nd_item<1> it) {
+                 int i = it.get_global_linear_id();
+                 int j = it.get_local_linear_id();
+
+                 slm[j] = 0;
+                 it.barrier(sycl::access::fence_space::local_space);
+
+                 for (int m = 0; m < 1024 * 1024; m++) {
+                   slm[j] += i * m;
+                   it.barrier(sycl::access::fence_space::local_space);
+                 }
+
+                 data[i] = slm[j];
+        });
+      }).wait();
+
+      auto end = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<double, std::milli> duration = end - start;
+      std::cout << "Execution Time: " << duration.count() << " ms" << std::endl;
+    }
+
+    std::cout << "Bank conflicts" << std::endl;
+    for (int bank_count = 0; bank_count <= 32; bank_count++) {
         auto start = std::chrono::high_resolution_clock::now();
 
         q.submit([&](handler &h) {
-            sycl::local_accessor<int, 1> slm(sycl::range(64 * 64), h);
+            sycl::local_accessor<int, 1> slm(sycl::range(32 * 64), h);
 
             h.parallel_for(sycl::nd_range(sycl::range{GLOBAL}, sycl::range{LOCAL}),
                    [=](sycl::nd_item<1> it) {
@@ -28,40 +86,17 @@ int main() {
                      int j = it.get_local_linear_id();
 
 
-                     slm[j*bank_count] = data[i];
+                     slm[j*bank_count] = 0;
                      it.barrier(sycl::access::fence_space::local_space);
-
+    
                      for (int m = 0; m < 1024 * 1024; m++) {
                        slm[j*bank_count] += i * m;
                        it.barrier(sycl::access::fence_space::local_space);
                      }
-
-
-                     data[i] += slm[j*bank_count];
+    
+                     data[i] = slm[j*bank_count];
 
             });
-
-
-            /*
-            // no-conflicts
-            h.parallel_for(sycl::nd_range(sycl::range{N}, sycl::range{32}),
-                   [=](sycl::nd_item<1> it) {
-                     int i = it.get_global_linear_id();
-                     int j = it.get_local_linear_id();
-
-                     slm[j] = 0;
-                     it.barrier(sycl::access::fence_space::local_space);
-
-                     for (int m = 0; m < 1024 * 1024; m++) {
-                       slm[j] += i * m;
-                       it.barrier(sycl::access::fence_space::local_space);
-                     }
-
-                     data[i] = slm[j];
-           });
-           */
-
-
         }).wait();
 
         auto end = std::chrono::high_resolution_clock::now();
